4169c9da909e4e6a07af42bd739a81d57df739fe
heyongqi10 <heyongqi10@jd.com>
2026-02-28 18:26:27 +0800

    feat: add @vercel/analytics dependency and integrate Analytics component
    - Added @vercel/analytics version 1.6.1 to dependencies in package.json.
    - Updated pnpm-lock.yaml to include the new dependency and its resolution details.
    - Integrated Analytics component into the RootLayout for enhanced analytics tracking.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import "./fonts.css";
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased">{children}</body>
+      <Analytics />
       <SpeedInsights />
     </html>
   );
